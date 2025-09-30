dc5eff4b4a26397f424c1417ef71f47494608f4e
Mariaapalacios <mp352@njit.edu>
2025-09-30 14:33:35 -0400

    post done

--- src/index.js
+++ src/index.js
@@ -5,6 +5,33 @@ const port = 3000;
 // Middleware to parse JSON bodies
 app.use(express.json());
 
+//Part 1: Create an Usee
+//define a POST route 
+app.post('/users', (req, res) => {
+    console.log("Received requested data using POST Method",req.body);
+
+    //process the data from request body
+    const {name,email}= req.body;
+
+    //send a response back to client
+    res.status(201).json({
+        message: 'User created successfully',
+        receivedData: {name,email}
+    })
+    if (!name || !email) {
+        return res.status(400).json({ error: 'Name and email are required' });
+    }
+
+    const upcomingUser = { id: uuidv4(), name, email };
+    users.push(upcomingUser);
+    res.status(201).json(upcomingUser);
+});
+
+//Part 2: Get all users
+app.get
+
+
+
 // **************************************************************
 // Put your implementation here
 // If necessary to add imports, please do so in the section above
@@ -12,6 +39,14 @@ app.use(express.json());
 app.get('/', (req, res) => {
     res.send('Hello World!');
 });
+
+
+
+
+
+
+
+
 
 // Do not touch the code below this comment
 // **************************************************************
